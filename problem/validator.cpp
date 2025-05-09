2f9be5a59af27b171c950abaf56d2b337bcf5ab4
Roberto Sales <rsalesc@Robertos-MacBook-Pro.local>
2025-05-09 14:20:57 -0300

    First preset commit.

---
+++ problem/validator.cpp
@@ -0,0 +1,16 @@
+#include "testlib.h"
+#include "rbx.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	registerValidation(argc, argv);
+
+	int MAX_N = getVar<int>("MAX_N"); // Read from package vars.
+
+	inf.readInt(1, MAX_N, "A");
+	inf.readSpace();
+	inf.readInt(1, MAX_N, "B");
+	inf.readEoln();
+	inf.readEof();
+}
